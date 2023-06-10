d28e72cde785cd2db124074845bdee2a664fc759
Romeo Siwach <romeosiwach@gmail.com>
2023-06-10 15:43:59 +0530

    This is the final update

---
+++ genesis.js
@@ -0,0 +1,8 @@
+const genesis_info = {
+    timestamp: Date.now(),
+    previousHash: '1747e5e9de9e6935f00a13c56ef1183e0c6129a3fd5b1bc1b25117481fb01453',
+    currentHash: '3626686820155647ca445ade86304ab15feba9dfc74b18fe6731adbaa8e34071',
+    data: 'Block Number : 0'
+};
+
+module.exports = {genesis_info};
